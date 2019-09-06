92081b7a388414a38d86297762411eb59db1d058
Ashley Devon <ashleychanteldevon@gmail.com>
2019-09-06 04:43:58 +0000

    create update destroy

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,6 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  
   def index
     @places = Place.all
   end
@@ -15,10 +14,23 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
 end
