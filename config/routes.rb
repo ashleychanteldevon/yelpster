e45f7835ae5251e9666ae8cdf7907dab67e79b05
Ashley Devon <ashleychanteldevon@gmail.com>
2019-10-04 15:35:03 +0000

    cleaning up

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-Nomster::Application.routes.draw do
-  devise_for :users
-  root 'places#index'
-  resources :places do
-    resources :comments, :photos, :only => :create
-  end
-  resources :users, :only => :show
-
-  resources :places
-  resources :photos
-=======
- Rails.application.routes.draw do
+Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'places#index'
@@ -18,5 +6,4 @@ Nomster::Application.routes.draw do
     resources :comments, :photos, only: :create
   end
   resources :users, only: :show
->>>>>>> cb739f71bc7044129a0aa6c9fdb21e7f3c049805
 end
